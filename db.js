91ff9da85088adea0e6de955b176533c55f170e2
Hemant Koli <hemantkoli@Hemants-MacBook-Pro.local>
2022-04-07 22:42:45 +0530

    Basic Car-Rental

---
+++ db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+const URL = process.env.MONGO_URL
+
+function connectDB(){
+
+    mongoose.connect(URL , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
